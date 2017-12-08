cc3cc84efe3665b6d1668e31ce48bd7d7321e8a7
Victor Horton <victorahorton@yahoo.com>
2017-12-08 09:28:13 -0500

    First commit

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyDNuU1WkrcZn44h4Yu_BoyOO878vbnBGfU",
+    authDomain: "catch-of-the-day-victor-horton.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-victor-horton.firebaseio.com",
+});
+
+export default base;
